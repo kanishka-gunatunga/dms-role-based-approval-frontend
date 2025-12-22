80ad0c6f4e8ff89ffb1eb50234b1ba2e7636fc11
madushan10 <mnmnjith@gmail.com>
2025-12-22 11:03:15 +0530

    license check and apply view 4

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
     {
       source: '/(.*)',
       headers: [
-        { key: 'Access-Control-Allow-Origin', value: 'https://dms-demo-2.vercel.app' },
+        { key: 'Access-Control-Allow-Origin', value: 'https://dms-role-based-approval-frontend.vercel.app' },
         { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
         { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
         { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
